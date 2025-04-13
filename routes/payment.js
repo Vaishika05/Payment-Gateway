aad257c87bf03c1829d1e9a9f7af0f86cdc7900a
Vaishika05 <agrawalvaishika5@gmail.com>
2025-04-14 00:28:30 +0530

    Retry logic implemented

--- routes/payment.js
+++ routes/payment.js
@@ -11,30 +11,48 @@ router.post("/", async (req, res) => {
         return res.status(400).json({ message: "Idempotency key is required." });
     }
 
-    try {
-        //Check if this idempotency key was already used
-        const existingPayment = await Payment.findOne({ idempotencyKey });
+    const retryTries = 3;
+    let currentAttempt = 0;
+    let delay = 1000;
+    const simulateFailure = () => {
+        throw new Error("Simulated DB error");
+    };
 
-        if (existingPayment) {
-            return res.status(200).json({
-                message: "Payment already processed",
-                status: existingPayment.status,
-                data: existingPayment,
+    while (currentAttempt < retryTries) {
+        try {
+            // Check if this idempotency key was already used
+            const existingPayment = await Payment.findOne({ idempotencyKey });
+
+            if (existingPayment) {
+                return res.status(200).json({
+                    message: "Payment already processed",
+                    status: existingPayment.status,
+                    data: existingPayment,
+                });
+            }
+            simulateFailure();
+            const newTransaction = new Payment({
+                amount,
+                createdAt: Date.now(),
+                status: randomBoolean(),
+                idempotencyKey,
             });
+
+            await newTransaction.save();
+
+            return res.status(201).json({ message: "Payment processed", data: newTransaction });
+        } catch (error) {
+            currentAttempt++;
+            console.error(`Attempt ${currentAttempt} failed. Error: ${error.message}`);
+
+            if (currentAttempt >= retryTries) {
+                return res.status(500).json({ message: "Max retries reached. Request failed." });
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, delay));
+
+            delay *= 2;
         }
-
-        // Create a new payment
-        const newTransaction = new Payment({
-            amount,
-            createdAt: Date.now(),
-            status: randomBoolean(),
-            idempotencyKey,
-        });
-
-        await newTransaction.save();
-        res.status(201).json({ message: "Payment processed", data: newTransaction });
-    } catch (err) {
-        res.status(500).json({ error: "Failed to process payment", details: err.message });
     }
 });
 
